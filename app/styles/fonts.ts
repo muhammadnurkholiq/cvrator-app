581adce580c3cd06a68ddb621a400eb6bbcca094
Muhammad Nurkholiq <mnurkholiq114@gmail.com>
2024-08-13 23:54:21 +0700

    update first page

--- app/styles/fonts.ts
+++ app/styles/fonts.ts
@@ -1,3 +1,9 @@
-import { Inter } from "next/font/google";
+// Import the necessary functions from `next/font/google`
+import { Inter, Roboto_Slab } from "next/font/google";
 
+// Configure the fonts with the desired subsets and styles
 export const inter = Inter({ subsets: ["latin"] });
+export const robotoSlab = Roboto_Slab({
+  subsets: ["latin"],
+  weight: ["400", "700"]
+});
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,58 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))"
+      screens: {
+        sm: "480px",
+        md: "768px",
+        lg: "976px",
+        xl: "1440px"
+      },
+      colors: {
+        primary: {
+          light: "#FFC1BB",
+          main: "#DD1A1A",
+          dark: "#790A00",
+          contrastText: "#FFFFFF"
+        },
+        blood: {
+          10: "#E9CECC",
+          20: "#FFC1BB",
+          30: "#CD4F5D",
+          40: "#C32839",
+          50: "#BD081C",
+          60: "#790A00",
+          70: "#610800",
+          80: "#490600",
+          90: "#300400",
+          100: "#1D0200"
+        },
+        grey: {
+          0: "#F3F3F3",
+          100: "#E7E7E7",
+          200: "#D1D1D1",
+          300: "#C7C7C7",
+          400: "#888888",
+          500: "#6D6D6D",
+          600: "#5D5D5D",
+          700: "#4F4F4F",
+          800: "#3D3D3D",
+          900: "#28292A",
+          A100: "#1F1F1F"
+        },
+        background: {
+          main: "#1F1F1F"
+        }
       }
     }
   },
-  plugins: []
+  plugins: [
+    function ({ addBase, theme }: any) {
+      addBase({
+        ":root": {
+          "--background-color-main": theme("colors.background.main")
+        }
+      });
+    }
+  ]
 };
 export default config;
