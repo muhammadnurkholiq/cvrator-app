5301869d13195106f10fcec753897fa5ed6f6d1d
Muhammad Nurkholiq <mnurkholiq114@gmail.com>
2024-08-14 00:31:05 +0700

    update background aceternity-ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,20 @@
 import type { Config } from "tailwindcss";
 
+const {
+  default: flattenColorPalette
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars
+  });
+}
+
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -53,6 +68,7 @@ const config: Config = {
     }
   },
   plugins: [
+    addVariablesForColors,
     function ({ addBase, theme }: any) {
       addBase({
         ":root": {
