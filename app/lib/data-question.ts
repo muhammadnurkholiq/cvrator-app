808ec1387b96b883c332dfa1567e31a7701cd36f
Muhammad Nurkholiq <mnurkholiq114@gmail.com>
2024-08-16 21:31:16 +0700

    update desc

--- app/lib/data-question.ts
+++ app/lib/data-question.ts
@@ -3,43 +3,43 @@ import { QuestionType } from "../types/question";
 export const questions: QuestionType[] = [
   {
     text: "Hi, What is your complete name?",
-    desc: "Use your full legal name to ensure consistency and professionalism in your resume.",
+    desc: "Example of input : John Doe",
     type: "text",
     line: "single"
   },
   {
     text: "Please input your email, phone number, and address",
-    desc: "Provide your current contact details so potential employers can easily reach you.",
+    desc: "Example of input : johndoe@gmail.com, 081234567890, Bandung Indonesia",
     type: "text",
     line: "multi"
   },
   {
     text: "What skills do you have that can be used professionally? Please list them.",
-    desc: "Highlight key skills that are relevant to the job you're applying for. This can include both technical and soft skills.",
+    desc: "Example of input : Writing,Communication..etc",
     type: "text",
     line: "multi"
   },
   {
     text: "Do you have any previous work experience? Please mention your most recent role.",
-    desc: "Include your last job title, company name, and the duration of your employment to give employers an overview of your work history.",
+    desc: "Example of input : Digital Marketing-Netflix-2023/2024, Advertising-Startup Company-2022/2023,.. etc",
     type: "text",
     line: "multi"
   },
   {
     text: "Please write down the education that you have completed.",
-    desc: "List your educational qualifications, starting from the most recent. Include the degree obtained, institution, and graduation year.",
+    desc: "Example of input : Informatics Engineer-Harvard-2014/2018, Science Major-Senior High School-2011/2014,.. etc",
     type: "text",
     line: "multi"
   },
   {
     text: "Do you have any certifications from training or institutes?",
-    desc: "Mention any relevant certifications that add value to your resume. Include the name of the certification, the issuing authority, and the date obtained.",
+    desc: "Example of input : Professional Engineering-Engineering Institute-80%, Professional finance-Finance Institute-100%...etc",
     type: "text",
     line: "multi"
   },
   {
     text: "If you have a portfolio website, please mention it.",
-    desc: "A portfolio website is a great way to showcase your work. Provide the link if you have one.",
+    desc: "Example of input : https://www.google.co.id/",
     type: "text",
     line: "multi"
   },
