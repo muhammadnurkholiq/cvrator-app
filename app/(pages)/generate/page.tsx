6bfe8cca44734f8c84543b5863adf287bf5cdc5e
Muhammad Nurkholiq <mnurkholiq114@gmail.com>
2024-08-18 18:00:56 +0700

    update

--- app/(pages)/generate/page.tsx
+++ app/(pages)/generate/page.tsx
@@ -2,26 +2,10 @@ import React from "react";
 import PDFGenerate from "@/app/components/pdf";
 import CircleLoading from "@/app/components/loading/circle-loading";
 import { UserData } from "@/app/types/user-data";
-
-async function fetchUserData(): Promise<UserData> {
-  const response = await fetch(
-    `${process.env.NEXT_APP_URL}/goal/response.json`
-  );
-  if (!response.ok) {
-    throw new Error("Failed to fetch data");
-  }
-  const data = await response.json();
-  return data.message;
-}
+import Response from "@/app/goal/response.json";
 
 const Page: React.FC = async () => {
-  let dataUser: UserData | null = null;
-
-  try {
-    dataUser = await fetchUserData();
-  } catch (error) {
-    console.error("Failed to fetch data:", error);
-  }
+  const dataUser: UserData | null = Response.message;
 
   if (!dataUser) {
     return <CircleLoading />;
